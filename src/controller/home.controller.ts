05b45c98440636b931886487629a9d3049321052
kenny <879464294@qq.com>
2023-12-27 19:59:26 +0800

    feat: add git.service

--- src/controller/home.controller.ts
+++ src/controller/home.controller.ts
@@ -14,8 +14,8 @@ export class HomeController {
 
   @Get('/pull')
   async pull(): Promise<string> {
-    await this.git.pull();
+    const res = await this.git.pull();
 
-    return 'success';
+    return res;
   }
 }
