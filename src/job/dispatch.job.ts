9ed74ba28f0b0d329dd0ab5651a2dfe7a66a50c8
kenny <879464294@qq.com>
2024-01-12 08:19:27 +0800

    chore: chore

--- src/job/dispatch.job.ts
+++ src/job/dispatch.job.ts
@@ -42,7 +42,8 @@ export class DispatchJobBili implements IJob {
     const realTime = dayjs().format('YYYY-MM-DD');
     // console.log('AT-[ realTime &&&&&********** ]', realTime);
 
-    const newStr = dutyEnv.replace(/2023-\d{2}-\d{2}/gi, realTime);
+    const newStr = dutyEnv.replace(/\d{4}-\d{2}-\d{2}/gi, realTime);
+
     console.log('AT-[ newStr &&&&&********** ]', newStr);
 
     const formatedStr = await prettier.format(newStr, {
