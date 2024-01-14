e7d43842c200a46e9f990b01f3871deaa36e5baf
kenny <879464294@qq.com>
2024-01-14 23:52:33 +0800

    feat: group数据格式更新

--- src/controller/api.controller.ts
+++ src/controller/api.controller.ts
@@ -44,11 +44,9 @@ export class APIController {
 
     const list = glob.sync(path.join(desktop, 't_*/*/'), { windowsPathsNoEscape: true });
 
-    list.map(item => item.replace('\\', '/').replace(desktop.replace('\\', '/'), ''));
-
     // console.log('lodash', lodash);
     const group = lodash.groupBy(
-      list.map(item => item.replace('\\', '/').replace(desktop.replace('\\', '/'), '')),
+      list.map(item => item.replace(/\\/gi, '/').replace(desktop.replace(/\\/gi, '/'), '')),
       path.dirname
     );
 
